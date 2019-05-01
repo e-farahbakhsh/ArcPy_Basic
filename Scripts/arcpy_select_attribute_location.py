207f0476f321681e45aae8528cb6cf18056d09fd
Ehsan Farahbakhsh <35557957+e-farahbakhsh@users.noreply.github.com>
2019-05-02 00:52:59 +0430

    Update arcpy_select_attribute_location.py

--- Scripts/arcpy_select_attribute_location.py
+++ Scripts/arcpy_select_attribute_location.py
@@ -3,10 +3,10 @@
 # Syntax: SelectLayerByAttribute_management (in_layer_or_view, {selection_type}, {where_clause})
 # ----------------------------------------------------------------------------------------------
 # import arcpy
-# arcpy.env.workspace = "D:/Python/Test/Vector"
-# arcpy.MakeFeatureLayer_management("Geology_StudyArea.shp", "Geology_StudyArea_Layer")
-# arcpy.SelectLayerByAttribute_management("Geology_StudyArea_Layer", "NEW_SELECTION", "Geo_unit = 'Ev'")
-# arcpy.CopyFeatures_management("Geology_StudyArea_Layer", "Geology_StudyArea_Layer_Ev")
+# arcpy.env.workspace = "path"
+# arcpy.MakeFeatureLayer_management("fileName", "layerName")
+# arcpy.SelectLayerByAttribute_management("layerName", "NEW_SELECTION", "fieldName = 'Value'")
+# arcpy.CopyFeatures_management("layerName", "fileName1")
 # ------------------------------------------------------------------------------------
 # Selecting by location
 
@@ -14,7 +14,7 @@
 #                                          {selection_type}, {invert_spatial_relationship})
 # -----------------------------------------------------------------------------------------
 import arcpy
-arcpy.env.workspace = "D:/Python/Test/Vector"
-arcpy.MakeFeatureLayer_management("Mineralization_StudyArea.shp", "Mineralization_StudyArea_Layer")
-arcpy.SelectLayerByLocation_management("Mineralization_StudyArea_Layer", "INTERSECT", "Geology_StudyArea_Layer_Ev.shp")
-arcpy.CopyFeatures_management("Mineralization_StudyArea_Layer", "Mineralization_StudyArea_Layer_Ev")
+arcpy.env.workspace = "path"
+arcpy.MakeFeatureLayer_management("fileName", "layerName")
+arcpy.SelectLayerByLocation_management("layerName", "INTERSECT", "fileName1")
+arcpy.CopyFeatures_management("layerName", "fileName2")
