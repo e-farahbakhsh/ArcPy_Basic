651217e88939f5e9130ff3ea9d61be94a7e149fa
Ehsan Farahbakhsh <35557957+e-farahbakhsh@users.noreply.github.com>
2019-05-08 23:53:08 +0430

    Update arcpy_overlay.py

--- Scripts/arcpy_overlay.py
+++ Scripts/arcpy_overlay.py
@@ -7,24 +7,24 @@
 # import arcpy
 # from arcpy import env
 # from arcpy.sa import *
-# env.workspace = "D:/Python/Test/Raster"
+# env.workspace = "path"
 #
-# outWeightedSum = WeightedSum(WSTable([["Th_UTM40N.tif", "VALUE", 0.5], ["U_UTM40N.tif", "VALUE", 0.5]]))
-# outWeightedSum.save("D:/Python/Test/Raster/Th_U.tif")
+# outWeightedSum = WeightedSum(WSTable([["fileName1", "VALUE", 0.5], ["fileName2", "VALUE", 0.5]]))
+# outWeightedSum.save("path1")
 # -----------------------------------------------------
 # Stand-alone script
 #
 # import arcpy
 # from arcpy import env
 # from arcpy.sa import *
-# env.workspace = "D:/Python/Test/Raster"
+# env.workspace = "path"
 #
-# inRaster1 = "Th_UTM40N.tif"
-# inRaster2 = "U_UTM40N.tif"
+# inRaster1 = "fileName1"
+# inRaster2 = "fileName2"
 # WSumTableObj = WSTable([[inRaster1, "VALUE", 0.5], [inRaster2, "VALUE", 0.5]])
 #
 # outWeightedSum = WeightedSum(WSumTableObj)
-# outWeightedSum.save("D:/Python/Test/Raster/Th_U.tif")
+# outWeightedSum.save("path1")
 # -----------------------------------------------------
 # Overlaying several rasters using a common measurement scale and weighting each according to its importance
 #
@@ -35,22 +35,22 @@
 # import arcpy
 # from arcpy import env
 # from arcpy.sa import *
-# env.workspace = "D:/Python/Test/Raster"
+# env.workspace = "path"
 #
-# outsuit = WeightedOverlay(WOTable([["Th_Reclassified.tif", 50, 'VALUE', RemapValue([[1, 2], [2, 3], [3, 4], ["NODATA", "NODATA"]])],
-# 								   ["U_Reclassified.tif", 50, 'VALUE', RemapValue([[1, 2], [2, 3], [3, 4], [4, 5], ["NODATA", "NODATA"]])]],
+# outsuit = WeightedOverlay(WOTable([["fileName1", 50, 'VALUE', RemapValue([[1, 2], [2, 3], [3, 4], ["NODATA", "NODATA"]])],
+# 								   ["fileName2", 50, 'VALUE', RemapValue([[1, 2], [2, 3], [3, 4], [4, 5], ["NODATA", "NODATA"]])]],
 # 								  [1, 9, 1]))
-# outsuit.save("D:/Python/Test/Raster/Th_U.tif")
+# outsuit.save("path1")
 # ----------------------------------------------
 # Stand-alone script
 #
 import arcpy
 from arcpy import env
 from arcpy.sa import *
-env.workspace = "D:/Python/Test/Raster"
+env.workspace = "path"
 
-inRaster1 = "Th_Reclassified.tif"
-inRaster2 = "U_Reclassified.tif"
+inRaster1 = "fileName1"
+inRaster2 = "fileName2"
 
 remap1 = RemapValue([[1, 2], [2, 3], [3, 4], ["NODATA", "NODATA"]])
 remap2 = RemapValue([[1, 2], [2, 3], [3, 4], [4, 5], ["NODATA", "NODATA"]])
@@ -58,4 +58,4 @@ remap2 = RemapValue([[1, 2], [2, 3], [3, 4], [4, 5], ["NODATA", "NODATA"]])
 myWOTable = WOTable([[inRaster1, 50, "VALUE", remap1],
                      [inRaster2, 50, "VALUE", remap2]], [1, 9, 1])
 outWeightedOverlay = WeightedOverlay(myWOTable)
-outWeightedOverlay.save("D:/Python/Test/Raster/Th_U.tif")
+outWeightedOverlay.save("path1")
